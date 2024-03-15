fea31579b0254e439b6e75f4ac7fef1d4f745f02
omar <omar@DESKTOP-55MD10R>
2024-03-15 10:29:21 +0100

    l2 print

--- Proyecto1/src/Paquete1/Principal2.java
+++ Proyecto1/src/Paquete1/Principal2.java
@@ -3,7 +3,8 @@ package Paquete1;
 public class Principal2 {
 
 	public static void main(String[] args) {
-		System.out.println("11");
+		System.out.println("l1");
+		System.out.println("l2");
 
 	}
 
